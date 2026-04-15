b99577b8873c343be62daae3d4196dd7b08d7a15
kobayu858 <yutaro.kobayashi.2@tier4.jp>
2026-04-15 21:48:47 +0900

    refactor: minor
    Signed-off-by: kobayu858 <yutaro.kobayashi.2@tier4.jp>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pydot
 
-# NetworkX が pydot 3.0 以降で AttributeError を起こす問題への暫定パッチ
 if not hasattr(pydot, 'quote_id_if_necessary') and hasattr(pydot, 'quote_if_necessary'):
     pydot.quote_id_if_necessary = pydot.quote_if_necessary
+
